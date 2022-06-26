ad8e6662cd36eff933bbb3597ed237abf816d375
mohamedrashad123 <mohamedrashad@dipdux.com>
2022-06-26 04:47:25 +0200

    uoload files

--- src/constant/const.js
+++ src/constant/const.js
@@ -1,5 +1,4 @@
-// export const baseURL = 'https://english4kidsapi.dipdux.com';
+export const baseURL = 'https://english4kidsapi.dipdux.com';
 // export const cdnLink = 'http://127.0.0.1:5050';
-
-export const baseURL = 'http://localhost:3000';
+// export const baseURL = 'http://localhost:3000';
 export const cdnLink = 'https://cdn-english4kids.dipdux.com';
